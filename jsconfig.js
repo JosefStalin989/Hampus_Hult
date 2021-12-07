9a3f89b795e81eb8953e63d6f6db384d5695ea4c
JosefStalin989 <hampus.hult@elev.ga.ntig.se.com>
2021-12-07 14:39:56 +0100

    update

---
+++ jsconfig.js
@@ -0,0 +1,29 @@
+const products = [
+{
+    id: 0,
+    name: "The Mucfee",
+    price: 650,
+    instock: 100, 
+    description: 
+    "Some shit computer",
+    imgSrc: "./img/laptop1.png",
+},
+{
+    id: 1,
+    name: "The Muc Eanrain",
+    price: 1400,
+    instock: 100,
+    description: 
+    "Some shit computer",
+    imgSrc: "./img/laptop5.png",
+},
+{
+    id: 2, 
+    name: "Trinity",
+    price: 1400,
+    instock: 100,
+    description:
+    "Some Shit computer",
+    imgSrc: "./img/Trio.png",
+}
+]
